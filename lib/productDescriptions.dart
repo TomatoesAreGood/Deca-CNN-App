9c3cc6974eefa3cbb230c5af8f39b937f7c5a693
TomatoesAreGood <139704217+TomatoesAreGood@users.noreply.github.com>
2025-02-05 18:46:24 -0500

    images

--- lib/productDescriptions.dart
+++ lib/productDescriptions.dart
@@ -7,12 +7,19 @@ class ProductDescriptions {
     3: "BIC Cristal Stic Ballpoint Pen M",
     4: "BIC Mechanical Pencil 0.7mm #2",
     5:" Paper Mate Intro Highlighter",
-    6:"Staples Hype! Pen-Style Highlighter",
+    6: "Staples Hype! Pen-Style Highlighter",
     7: "Black Sharpie Fine Tip Permanent Marker",
     8: "Pentel Rolly C3 BallPoint Pen",
     9: "Casemate Hb #2 Yellow Wood Pencil",
     10: "Paper Mate Ballpoint Pen M"
   }; 
 
+/*
+https://www.staples.ca/products/35472-en-sharpie-chisel-tip-pocket-style-highlighters-assorted-colours-4-pack?listId=search
+https://www.staples.ca/products/412775-en-staples-hype-chisel-tip-pen-style-highlighters-yellow-6-pack
+https://www.staples.ca/products/11520-en-sharpie-permanent-markers-fine-tip-black-12-pack
+https://www.staples.ca/products/2675867-en-bic-4-colour-3-1-pen-and-mechanical-pencil
+https://www.staples.ca/products/603283-en-staples-wood-cased-pencils-2-hb-72-pack 
+ */
 
 }
